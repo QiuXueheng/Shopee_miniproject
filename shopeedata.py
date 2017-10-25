5ca0208e9fb3745354dec654e6ebe61d1de09c18
QiuXueheng <33094837+QiuXueheng@users.noreply.github.com>
2017-10-25 23:48:49 +0800

    Add files via upload

---
+++ shopeedata.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import dateutil
+from datetime import datetime
+
+
+def view_log_read(path, lags):
+    action1 = pd.read_csv(path)
+
+    action1_tempdummy = pd.get_dummies(action1['event_name'], prefix='%d' % lags)
+
+    action1_frame = pd.merge(action1, action1_tempdummy, left_index=True, right_index=True)
+
+    for idx in range(action1_frame.shape[0]):
+        for act in action1_tempdummy.columns.values.tolist():
+            if action1_frame.get_value(idx, act) == 1:
+                action1_frame.set_value(idx, act, action1_frame.get_value(idx, 'count'))
+
+    action1_frame = action1_frame.drop(['event_name', 'count'], axis=1)
+
+    if lags > 0:
+        lag_label = '%d_day_before' % lags
+
+        action1_frame = action1_frame.groupby(['userid', lag_label]).sum().reset_index()
+
+        action1_frame[lag_label] = pd.to_datetime(action1_frame[lag_label])
+
+        for idx in range(action1_frame.shape[0]):
+            action1_frame.set_value(idx, lag_label,
+                                    action1_frame.get_value(idx, lag_label) + dateutil.relativedelta.relativedelta(
+                                        days=lags))
+
+        action1_frame[lag_label] = action1_frame[lag_label].dt.strftime('%Y-%m-%d')
+        action1_frame = action1_frame.rename(columns={lag_label: 'voucher_received_date'})
+    else:
+        action1_frame = action1_frame.groupby(['userid', 'voucher_received_date']).sum().reset_index()
+
+    print('log file %d has been imported' % lags)
+    return action1_frame
+
+
+def time_encode(dataframe):
+    shopeedata = dataframe
+    for idx in range(shopeedata.shape[0]):
+        unixtime = datetime.strptime(shopeedata.get_value(idx, 'registration_time'), '%Y-%m-%d %H:%M:%S').timestamp()
+        shopeedata.set_value(idx, 'registration_time', unixtime)
+        if pd.isnull(shopeedata.get_value(idx, 'birthday')):
+            shopeedata.set_value(idx, 'birthday', datetime(2038, 1, 1).timestamp())
+        elif datetime.strptime(shopeedata.get_value(idx, 'birthday'), '%Y-%m-%d') < datetime(1970, 1, 3):
+            shopeedata.set_value(idx, 'birthday', datetime(1970, 1, 3).timestamp())
+        else:
+            unixtime2 = datetime.strptime(shopeedata.get_value(idx, 'birthday'), '%Y-%m-%d').timestamp()
+            shopeedata.set_value(idx, 'birthday', unixtime2)
+    return shopeedata
+
+
+trainframe = pd.read_csv(r'C:\Users\Qiu\Desktop\Shopee\training.csv')
+user_profile = pd.read_csv(r'C:\Users\Qiu\Desktop\Shopee\user_profiles_MY.csv')
+likes = pd.read_csv(r'C:\Users\Qiu\Desktop\Shopee\likes.csv')
+vouchers = pd.read_csv(r'C:\Users\Qiu\Desktop\Shopee\Voucher_mechanics.csv')
+activedate = pd.read_csv(r'C:\Users\Qiu\Desktop\Shopee\voucher_distribution_active_date.csv')
+
+trainframe = pd.merge(trainframe, user_profile, left_on='userid', right_on='userid')
+
+trainframe = pd.merge(trainframe, vouchers, left_on='promotionid_received', right_on='promotionid_received')
+
+trainframe = pd.merge(trainframe, activedate,
+                      left_on=['userid', 'promotionid_received', 'voucher_code_received', 'voucher_received_time'],
+                      right_on=['userid', 'promotionid_received', 'voucher_code_received', 'voucher_received_time'])
+
+for lags in range(31):
+    log_temp = view_log_read(r'C:\Users\Qiu\Desktop\Shopee\view_log_%d.csv' % lags, lags)
+    log_temp.to_csv(r'.\view_log\view_log_%d.csv' % lags)
+    trainframe = pd.merge(trainframe, log_temp, left_on=['userid', 'voucher_received_date'],
+                          right_on=['userid', 'voucher_received_date'])
+
+trans_log = pd.read_csv(r'C:\Users\Qiu\Desktop\Shopee\transactions_MY.csv')
+for idx in range(trans_log.shape[0]):
+    for col in ['voucher_code_used', 'promotionid_used']:
+
+        if pd.isnull(trans_log.get_value(idx, col)):
+            trans_log.set_value(idx, col, 0)
+        else:
+            trans_log.set_value(idx, col, 1)
+
+trans_log = trans_log.drop(['orderid', 'shopid', 'order_time'], axis=1)
+transaction = trans_log.groupby(['userid']).sum().reset_index()
+trainframe2 = pd.merge(trainframe, transaction, left_on=['userid'],
+                       right_on=['userid'], how='left')
+
+for idx in range(trainframe2.shape[0]):
+    if trainframe2.get_value(idx, 'total_price') == 0:
+        trainframe2.set_value(idx, 'promotionid_used', 0)
+
+trainframe3 = time_encode(trainframe2)
+
+traindata = trainframe3.drop(['userid', 'promotionid_received', 'voucher_code_received', 'voucher_received_date'],
+                             axis=1)
+traindata = traindata.fillna(0)
+
+traindata.to_csv('traindata.csv')
